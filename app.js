70f23b12e9174b63e32c639257dc882deae1d27d
hogesuke <miyado@gmail.com>
2016-12-18 19:04:32 +0900

    websocket clientを正常に動くように改修

--- app.js
+++ app.js
@@ -2,14 +2,34 @@
 
 const dash_button = require('node-dash-button');
 const config = require('./config/config.json');
-const io = require('socket.io-client');
+const WebSocketClient = require('websocket').client;
 
 const dash = dash_button('ac:63:be:14:b8:02', null, null, 'all');
-const socket = io.connect(config.ws_server_url);
+const sw_client = new WebSocketClient();
 
-let isPowerOn = false;
+let isPowerUp = false;
 
-dash.on('detected', () => {
-  console.log(config.mac_address);
-  socket.emit('power', isPowerOn ? 'power_off' : 'power_off');
+sw_client.on('connectFailed', function(error) {
+  console.log('Connection failure: ' + error.toString());
 });
+
+sw_client.on('connect', (con) => {
+  console.log('Connection Success!');
+
+  con.on('message', (message) => {
+    if (message.utf8Data === 'power_on') {
+      console.log('Successful Power On');
+      isPowerUp = true;
+    }
+    if (message.utf8Data === 'power_off') {
+      console.log('Successful Power Off');
+      isPowerUp = false;
+    }
+  });
+
+  dash.on('detected', () => {
+    con.send(isPowerUp ? 'power_off' : 'power_on');
+  });
+});
+
+sw_client.connect(config.ws_server_url);
